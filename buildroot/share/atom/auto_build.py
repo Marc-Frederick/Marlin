2fd709bde6d7af42045d5a89028161008cc0d4a2
Marc-Frédérick Blanchet <marc.frederick.blanchet@gmail.com>
2020-05-02 18:12:50 -0400

    Import changes from the bigtreetech v1.3 Marlin repository prior to commit 8f09d2e7904f8f743be149b6b213575e7f36ee29  (integration of bugfix branch in BTT repository 52932435d120b7007287dc7b01bb72a45ab64f65)

--- buildroot/share/atom/auto_build.py
+++ buildroot/share/atom/auto_build.py
@@ -599,16 +599,12 @@ def get_env(board_name, ver_Marlin):
           target_env = 'DUE_USB'
         else:
           target_env = 'DUE'
-    elif env_A == 'STM32F103RC_bigtree' or env_A == 'STM32F103RE_bigtree':
-      if env_A == 'STM32F103RE_bigtree':
-        get_answer(board_name, 'MCU Type?', 'STM32F103RC', 'STM32F103RE')
-        if 1 == get_answer_val:
-          env_A = 'STM32F103RC_bigtree'
-      target_env = env_A
-      if env_A == 'STM32F103RC_bigtree':
-        get_answer(board_name, 'RCT6 Flash Size?', '512K', '256K')
-        if 1 == get_answer_val:
-          target_env += '_512K'
+    elif env_A == 'STM32F103RC_bigtree':
+      get_answer(board_name, 'RCT6 Flash Size?', '512K', '256K')
+      if 1 == get_answer_val:
+        target_env = 'STM32F103RC_bigtree_512K'
+      else:
+        target_env = 'STM32F103RC_bigtree'
       get_answer(board_name, 'USB Support?', 'No USB', 'USB')
       if 1 == get_answer_val:
         target_env += '_NOUSB'
