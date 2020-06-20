640dfaee89c1c2d320844ae5c7f1ffa618bcb26e
Marc-Frédérick Blanchet <marc.frederick.blanchet@gmail.com>
2020-06-20 18:45:16 -0400

    Setup bed size and offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1120,16 +1120,16 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 235
-#define Y_BED_SIZE 235
+#define X_BED_SIZE 220
+#define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
-#define Y_MIN_POS 0
+#define X_MIN_POS -35
+#define Y_MIN_POS -10
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 250
+#define Z_MAX_POS 235
 
 /**
  * Software Endstops
